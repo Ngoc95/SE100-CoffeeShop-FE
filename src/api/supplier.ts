7972bbf31de0173a39f155bb8ad65eeebf641b58
NguyenThanhVu028 <23521811@gm.uit.edu.vn>
2026-01-21 01:13:54 +0700

    Implement deleting and editing supplier feature

--- src/api/supplier.ts
+++ src/api/supplier.ts
@@ -3,3 +3,36 @@ import axiosClient from './axiosClient'
 export const getSuppliers = (params?: Record<string, any>) => {
     return axiosClient.get('/suppliers', { params })
 }
+
+export const getSupplierCategories = (params?: Record<string, any>) => {
+    return axiosClient.get('/suppliers/categories', { params })
+}
+
+export const deleteSupplier = (id: number) => {
+    return axiosClient.delete(`/suppliers/${id}`)
+}
+
+export const updateSupplier = (
+    id: number,
+    name?: string,
+    contactPerson?: string,
+    phone?: string,
+    email?: string,
+    address?: string,
+    city?: string,
+    category?: string,
+    status?: string
+) => {
+    let params: Record<string, any> = {
+        "name": name,
+        "contactPerson": contactPerson,
+        "phone": phone,
+        "email": email,
+        "address": address,
+        "city": city,
+        "category": category,
+        "status": status
+    }
+    console.log("Edit params: ", params)
+    return axiosClient.patch(`/suppliers/${id}`, params)
+}
